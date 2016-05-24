24aeaa0bc4fb63d3114bec13c451c36de475614e
Sunghwan Kang <guru21@nate.com>
2016-05-24 15:04:55 +0900

    3장 완료

--- ObjC/main.m
+++ ObjC/main.m
@@ -8,10 +8,64 @@
 
 #import <Foundation/Foundation.h>
 
+//---- @interface 부분 ----
+@interface Fraction : NSObject
+-(void) print;
+-(void) setNumerator:   (int) n;
+-(void) setDenominator: (int) d;
+-(int) numerator;
+-(int)  denominator;
+
+@end
+
+//----@implementation 부분 ----
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+-(int)  numerator
+{
+    return numerator;
+}
+
+-(int)  denominator
+{
+    return denominator;
+}
+@end
+
+//---- 프로그램 부분 ----
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *myFraction;
+        
+        //Fraction 인스턴트를 생성한다.
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // 1/3로 분수의 값을 설정한다.
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        
+        //print 메서드로 분수의 값을 표시한다.
+        NSLog(@"The value of myFraction is : %i/%i", [myFraction numerator], [myFraction denominator]);
     }
     return 0;
 }
